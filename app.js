abad24a5a66980fb514fc25b8c13904dfa6b541e
ishaan-bhandari <ishaanbhandari0@gmail.com>
2024-08-03 18:38:16 -0500

    added app file

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { connectToDatabase } = require('./database');
+const knownFacesRoutes = require('./routes/knownFaces');
+const attendanceRoutes = require('./routes/attendance');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+connectToDatabase((db) => {
+    app.locals.db = db;
+
+    app.use('/knownFaces', knownFacesRoutes);
+    app.use('/attendance', attendanceRoutes);
+
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+});
